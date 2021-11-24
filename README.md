00ea58c767b1adb67979e9ccb59a722c9af5f4ff
ay13 <aycodes@gmail.com>
2021-11-23 21:31:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-wmshyj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-wmshyj)
---
+++ src/store/admins.js
@@ -0,0 +1,9 @@
+import { createDerived } from 'nanostores';
+
+import { users } from './users.js';
+
+const admins = createDerived(users, (list) =>
+  list.filter((user) => !user.isAdmin)
+);
+
+export { admins };
